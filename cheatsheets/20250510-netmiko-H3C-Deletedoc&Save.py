24147ef0d10a3fde9c319090fe7fee5466b94e74
mr_weidawei <mr_weidawei@hotmail.com>
2025-05-12 19:50:08 +0800

    增加关于Netmiko处理设备提示命令的注释

--- cheatsheets/20250510-netmiko-H3C-Deletedoc&Save.py
+++ cheatsheets/20250510-netmiko-H3C-Deletedoc&Save.py
@@ -6,15 +6,15 @@
 import netmiko
 from netmiko import ConnectHandler,file_transfer
 
-Switch2 = {
+Switch1 = {
     'device_type': 'hp_comware',
     'host': '11.0.0.2',
     'username': 'netops',
     'password': 'Admin@1234',
     'port': 22,
 }
-with ConnectHandler(**Switch2) as connect:
-    print("已经成功登录交换机"+Switch2['host'])
+with ConnectHandler(**Switch1) as connect:
+    print("已经成功登录交换机"+Switch1['host'])
 
     output = connect.send_command(command_string="delete flash:/text.txt",
                   expect_string=r"Delete flash:/text.txt?",
@@ -24,7 +24,15 @@ with ConnectHandler(**Switch2) as connect:
                   expect_string=r">",
                   strip_prompt=False,
                   strip_command=False)
-    
     print(output)
+'''
+通过ConnectHandler()登录Switch1后，前后两次调用send_command()函数，
+在第一个send_command()函数中通过command_string这个参数向Switch1输入命令del flash0:/test.txt，
+然后在expect_string这个参数里告知Netmiko去Switch1的回显内容里查找“Delete flash:/text.txt?”这段系统返回的提示命令，
+如果查到了的话，则继续输入命令y（第二个send_command()）让脚本删除test.txt这个文件，
+之后如果接收到命令提示符">"，则继续执行脚本后面的代码。
+strip_prompt和strip_command两个参数这里放Fasle就行，目的是让代码最后的print(output)输出的内容的格式更好看一点：
+''' 
+    
 
    
