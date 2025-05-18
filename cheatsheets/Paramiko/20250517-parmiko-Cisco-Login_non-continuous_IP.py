f6ecf803dd538b2ec102fef4ec7b24e8dd0a643e
15201681252 <mr_weidawei@163.com>
2025-05-18 21:54:44 +0800

    增加注释

--- cheatsheets/Paramiko/20250517-parmiko-Cisco-Login_non-continuous_IP.py
+++ cheatsheets/Paramiko/20250517-parmiko-Cisco-Login_non-continuous_IP.py
@@ -7,7 +7,6 @@ from getpass import getpass
 # 检查文件系统中是否存在名为"ip_list.txt"的文件，并打印结果
 print(os.path.isfile("ip_list.txt"))
 
-
 # 获取当前脚本的绝对路径
 script_path = os.path.abspath(__file__)
 
@@ -20,7 +19,6 @@ ip_list_path = os.path.join(script_directory, "ip_list.txt")
 # 检查 ip_list.txt 是否存在于当前脚本目录下，并打印结果
 print(os.path.isfile(ip_list_path))
 
-
 '''
 
 username = input("Enter your username: ")
@@ -28,10 +26,29 @@ password = getpass("Enter your password: ")
 
 f = open("ip_list.txt","r")
 for ip in f.readlines():
-    ip = ip.strip()
+    ip = ip.strip() #去除字符串 ip 两端的空白字符（如空格、换行、制表符等），并重新赋值给变量 ip
+
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(hostname=ip,username=username,password=password,look_for_keys=False)
+'''
+ssh_client = paramiko.SSHClient()
+这段代码创建了一个 SSHClient 对象，用于实现 SSH 连接。具体功能如下：
+1)paramiko.SSHClient() 是 Paramiko 库提供的类，用于建立和管理 SSH 连接。
+2)ssh_client 是该类的一个实例，后续可用于连接远程服务器、执行命令等操作。
+
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+自动接受所有未知的SSH主机密钥指纹，避免连接时因未保存的主机密钥而抛出异常。
+1)set_missing_host_key_policy()：设置当目标主机的密钥未在本地缓存时的处理策略
+2)paramiko.AutoAddPolicy()：自动将未知主机密钥添加到本地缓存中（不安全，适合测试环境）
+
+ssh_client.connect(hostname=ip,username=username,password=password,look_for_keys=False)
+该代码使用paramiko库的SSHClient对象连接远程SSH服务器。
+hostname=ip：指定目标主机的IP地址
+username=username：登录用户名
+password=password：登录密码
+look_for_keys=False：禁用自动查找本地私钥，避免尝试使用密钥认证
+'''
     print ("\n\nConnecting to device " + (ip))
     print ("Successfully connected to device ",ip)
     remote_connection = ssh_client.invoke_shell()
